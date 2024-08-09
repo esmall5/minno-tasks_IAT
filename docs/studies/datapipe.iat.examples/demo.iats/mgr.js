d5161ee7d0c951539888318c80499f5b9e5b7f47
esmall5 <emma_small@brown.edu>
2024-08-09 10:17:59 -0400

    Update mgr.js

--- docs/studies/datapipe.iat.examples/demo.iats/mgr.js
+++ docs/studies/datapipe.iat.examples/demo.iats/mgr.js
@@ -4,7 +4,7 @@ define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/da
 
     API.setName('mgr');
     API.addSettings('skip',true);
-	init_data_pipe(API, 'SJkEpXW56fCh',  {file_type:'csv'});	
+	init_data_pipe(API, 'MJGly1jNf9pUnhFIS5rOuqAktUzyfIZXVb81A3rsrD5S0Tr19GKFrggwp96cLID2kUn4OU',  {file_type:'csv'});	
 
 
     API.addTasksSet({
@@ -14,55 +14,11 @@ define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/da
             scriptUrl: 'choose.js'
         }],
 
-        age: [{
-            type: 'time',
-            name: 'age',
-            scriptUrl: 'age.js'
-        }],
-        arab: [{
-            type: 'time',
-            name: 'arab',
-            scriptUrl: 'arab.js'
-        }],
-        asian: [{
-            type: 'time',
-            name: 'asian',
-            scriptUrl: 'asian.js'
-        }],
-        disability: [{
-            type: 'time',
-            name: 'disability',
-            scriptUrl: 'disability.js'
-        }],
-        nativee: [{
-            type: 'time',
-            name: 'nativee',
-            scriptUrl: 'native.js'
-        }],
-        genderscience: [{
-            type: 'time',
-            name: 'genderscience',
-            scriptUrl: 'genderscience.js'
-        }],
-        sexuality: [{
-            type: 'time',
-            name: 'sexuality',
-            scriptUrl: 'sexuality.js'
-        }],
         weight: [{
             type: 'time',
             name: 'weight',
             scriptUrl: 'weight.js'
-        }],
-	hispanic: [{
-            type: 'time',
-            name: 'hispanic',
-            scriptUrl: 'hispanic.js'
-        }],
-        race: [{
-            type: 'time',
-            name: 'race',
-            scriptUrl: 'race.js'
+		
         }]
     });
 
@@ -77,42 +33,7 @@ define(['managerAPI', 'https://cdn.jsdelivr.net/gh/minnojs/minno-datapipe@1.*/da
 	                mixer:'multiBranch', 
 	                branches :
 	                [
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'age'}],
-	                        data:[{inherit:'age'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'arab'}],
-	                        data:[{inherit:'arab'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'asian'}],
-	                        data:[{inherit:'asian'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'disability'}],
-	                        data:[{inherit:'disability'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'genderscience'}],
-	                        data:[{inherit:'genderscience'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'native'}],
-	                        data:[{inherit:'nativee'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'race'}],
-	                        data:[{inherit:'race'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'sexuality'}],
-	                        data:[{inherit:'sexuality'}]
-	                    },
-	                    { 
-	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'hispanic'}],
-	                        data:[{inherit:'hispanic'}]
-	                    },
+	        
 	                    { 
 	                        conditions:[{compare: 'global.choose.questions.choice.response', to: 'weight'}],
 	                        data:[{inherit:'weight'}]
